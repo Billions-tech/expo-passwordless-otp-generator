f4d008d0968b54021aad004099a78f77c289278a
Tyson <abubash55@gmail.com>
2026-08-31 08:53:34 +0100

    yeah

--- eas-hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Patching dynamically generated Gradle configurations..."
-if [ -d "android" ]; then
-  echo 'org.gradle.kotlin.dsl.all.compiler.arguments=-Xskip-metadata-version-check' >> android/gradle.properties
-  echo "Gradle properties successfully updated with metadata skip flag."
-else
-  echo "Error: android directory not found yet."
-fi
